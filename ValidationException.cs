af5c87543687a17f50c7656d6f430277d265b1ca
Christian Schou <christian.schou@hotmail.com>
2024-02-16 22:03:42 +0100

    Add validation exception

---
+++ ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using GhostLyzer.Core.Validation.Models;
+
+namespace GhostLyzer.Core.Exceptions
+{
+    /// <summary>
+    /// Represents errors that occur during validation operations.
+    /// </summary>
+    public class ValidationException : CustomException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidationException"/> class with a specified validation result model.
+        /// </summary>
+        /// <param name="validationResultModel">The result of the validation operation.</param>
+        public ValidationException(ValidationResultModel validationResultModel)
+        {
+            ValidationResultModel = validationResultModel;
+        }
+
+        /// <summary>
+        /// Gets the result of the validation operation.
+        /// </summary>
+        public ValidationResultModel ValidationResultModel { get; }
+    }
+}
